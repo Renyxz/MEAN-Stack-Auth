bd342c82215a269e7315c4b316e04ad60235ff30
Ren <ren.yxz.dev@gmail.com>
2017-12-09 15:41:01 -0500

    Intergrated validate service and flash message into sign up function

--- client/client-angular-src/src/app/components/sign-up/sign-up.component.ts
+++ client/client-angular-src/src/app/components/sign-up/sign-up.component.ts
@@ -1,4 +1,8 @@
 import { Component, OnInit } from '@angular/core';
+import { FlashMessagesService } from 'angular2-flash-messages';
+import { ValidateService } from '../../services/validate.service';
+
+
 
 @Component({
   selector: 'app-sign-up',
@@ -12,7 +16,8 @@ export class SignUpComponent implements OnInit {
 
 
 
-  constructor() { }
+  constructor(private vs: ValidateService,
+              private fms: FlashMessagesService) { }
 
   ngOnInit() {
   }
@@ -21,17 +26,29 @@ export class SignUpComponent implements OnInit {
 
   // On sign up
   onSignUp() {
+
     // User object
     const user = {
       username: this.username,
       password: this.password
     };
 
+
     // Validate required fields
+    if (!this.vs.validateSignUp(user)) {
+      this.fms.show('Please complete all fields.', { cssClass: 'alert-danger' });
+      return false;
+    }
 
 
     // Validate email format
+    const email = user.username;
 
+    if (!this.vs.validateEmail(email)) {
+      this.fms.show('Invalid email format.', { cssClass: 'alert-danger' });
+      console.log('Invalid format');
+      return false;
+    }
 
   }
 
