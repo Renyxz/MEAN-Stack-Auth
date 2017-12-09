4618adc70fe920135cb37d714b4d722a92dd3ada
Ren <ren.yxz.dev@gmail.com>
2017-12-08 21:30:09 -0500

    Generated basic front-end components

--- client/client-angular-src/src/app/app.module.ts
+++ client/client-angular-src/src/app/app.module.ts
@@ -3,11 +3,23 @@ import { NgModule } from '@angular/core';
 
 
 import { AppComponent } from './app.component';
+import { DashboardComponent } from './components/dashboard/dashboard.component';
+import { HomeComponent } from './components/home/home.component';
+import { NavComponent } from './components/nav/nav.component';
+import { ProfileComponent } from './components/profile/profile.component';
+import { SignInComponent } from './components/sign-in/sign-in.component';
+import { SignUpComponent } from './components/sign-up/sign-up.component';
 
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    DashboardComponent,
+    HomeComponent,
+    NavComponent,
+    ProfileComponent,
+    SignInComponent,
+    SignUpComponent
   ],
   imports: [
     BrowserModule
