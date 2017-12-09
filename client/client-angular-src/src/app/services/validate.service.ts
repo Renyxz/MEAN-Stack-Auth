ec82f5ce5d091fc0f745079acebbab01cf47b61f
Ren <ren.yxz.dev@gmail.com>
2017-12-09 14:54:42 -0500

    Added functions for sign up input fields and email validation

---
+++ client/client-angular-src/src/app/services/validate.service.ts
@@ -0,0 +1,33 @@
+import { Injectable } from '@angular/core';
+
+@Injectable()
+export class ValidateService {
+
+  constructor() { }
+
+
+
+  // Validate input fields required for sign up
+  validateSignUp(user) {
+
+    switch (undefined) {
+      case user.username:
+      case user.password:
+        return false;
+
+        default:
+          return true;
+    }
+
+  }
+
+  // Validate email address
+  validateEmail(email) {
+    // Regular expression for testing email address
+    const reg = /^[a-zA-Z0-9.!#$%&’*+/=?^_`{|}~-]+@[a-zA-Z0-9-]+(?:\.[a-zA-Z0-9-]+)*$/;
+
+    return reg.test(email);
+  }
+
+
+}
