b6ad801757ada2bd2c22457a800fed89abd2b2bf
Ren <ren.yxz.dev@gmail.com>
2017-12-13 18:18:15 -0500

    Added validate service and will need to re-structure back-end code to implement signInUser

--- client/client-angular-src/src/app/components/sign-in/sign-in.component.ts
+++ client/client-angular-src/src/app/components/sign-in/sign-in.component.ts
@@ -1,4 +1,10 @@
 import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
+import { ValidateService } from '../../services/validate.service';
+import { AuthService } from '../../services/auth.service';
+import { FlashMessagesService } from 'angular2-flash-messages';
+
+
 
 @Component({
   selector: 'app-sign-in',
@@ -7,9 +13,57 @@ import { Component, OnInit } from '@angular/core';
 })
 export class SignInComponent implements OnInit {
 
-  constructor() { }
+  email: String;
+  password: String;
+
+  constructor(private router: Router,
+              private vs: ValidateService,
+              private as: AuthService,
+              private fms: FlashMessagesService) { }
 
   ngOnInit() {
+  }
+
+
+  // On sign in
+  onSignIn() {
+    const user = {
+      email: this.email,
+      password: this.password
+    };
+
+
+    // Validate required fields
+    if (!this.vs.validateInput(user)) {
+      this.fms.show('Please complete all fields to sign in.', { cssClass: 'alert-danger' });
+      return false;
+    }
+
+
+    // Validate email format
+    const email = user.email;
+    if (!this.vs.validateEmail(email)) {
+      this.fms.show('Incorrect email format.', { cssClass: 'alert-danger' });
+      return false;
+    }
+
+    // Sign in user
+    this.as.signInUser(user).subscribe( data => {
+      console.log(data);
+      // // On successful sign in:
+      // if (data) {
+
+      //   this.fms.show(data.msg, { cssClass: 'alert-success' });
+      //   this.router.navigate(['/dashboard']);
+
+      // } else {
+      //   // If sign in failed:
+      //   this.fms.show(data.msg, { cssClass: 'alert-danger' });
+      //   return false;
+      // }
+
+    });
+
   }
 
 }
